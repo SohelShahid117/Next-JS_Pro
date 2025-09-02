8aa2b2314cbd9eef64a099eacc1926c67088c2f3
SohelShahid117 <sohelshahid09@gmail.com>
2025-09-02 16:23:14 +0600

    672. Active Links Styling in Navigation done

--- layout-navigation/src/app/components/Navbar.tsx
+++ layout-navigation/src/app/components/Navbar.tsx
@@ -1,4 +1,6 @@
+"use client"
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 const Navbar = () => {
@@ -8,17 +10,27 @@ const Navbar = () => {
     { name: "contact", href: "/contact" },
     { name: "Blog", href: "/blog" },
   ];
+
+  const pathName = usePathname()
+  console.log(pathName)
   return (
     <header>
       <div className="flex justify-between max-w-7xl mx-auto my-5 border-b-3">
         <Link href="/">Logo</Link>
         <nav>
           <ul className="flex gap-5">
-            {navItems.map((item, i) => (
+            {navItems.map((item, i) =>     {
+              // const x = 5;
+              // console.log(x)
+
+              const isActive = pathName == item.href;
+              console.log(isActive)
+              return (
               <li key={i}>
-                <Link href={item.href}>{item.name}</Link>
+                <Link href={item.href} className={`px-3 py-1 text-gray-500 ${isActive?"text-orange-500 font-bold":""}`}>{item.name}</Link>
               </li>
-            ))}
+            )
+            })}
           </ul>
         </nav>
       </div>
