58142d566153e772c03ff86e9faf0083ff14da7d
SohelShahid117 <sohelshahid09@gmail.com>
2025-09-03 22:13:51 +0600

    675. Templates for Dynamic Pages 10 min done

---
+++ advanced-routing/src/app/components/Navbar.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <header>
+      <nav className="flex gap-4 bg-gray-800 text-white">
+        <Link href="/">Home</Link>
+        <Link href="/dashboard">Dashboard</Link>
+        <Link href="/dashboard/overview">Overview</Link>
+        <Link href="/dashboard/reports">Reports</Link>
+        <Link href="/dashboard/settings">Settings</Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
---
+++ advanced-routing/src/app/dashboard/reports/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>Report page</div>;
+};
+
+export default page;
