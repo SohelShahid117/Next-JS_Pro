82b04e202b8154118ef967ba0a8b0ec1fa553cd3
SohelShahid117 <sohelshahid09@gmail.com>
2025-09-12 23:53:33 +0600

    678. Recovering Errors, Handling Errors in Nested Routes done

--- advanced-routing/src/app/dashboard/xyz/page.tsx
+++ advanced-routing/src/app/dashboard/xyz/page.tsx
@@ -1,13 +1,12 @@
 "use client";
-
 import React, { useEffect } from "react";
 
-const ABC = () => {
+const XYZ = () => {
   useEffect(() => {
     //throw new Error("the Error in ABC");
-    throw new Error("the Error in ABC");
+    throw new Error("the Error in XYZ");
   }, []);
-  return <div>ABC</div>;
+  return <div>XYZ</div>;
 };
 
-export default ABC;
+export default XYZ;
