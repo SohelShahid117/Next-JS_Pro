cb8199db40181764a88d36db4a29adf455014766
SohelShahid117 <sohelshahid09@gmail.com>
2025-09-18 13:10:51 +0600

    682. Conditional Routes done

--- advanced-routing/src/app/dashboard/layout.tsx
+++ advanced-routing/src/app/dashboard/layout.tsx
@@ -1,7 +1,13 @@
+import { redirect } from 'next/navigation';
 import React from 'react'
 
 const DashboardLayout = ({children}:{children:React.ReactNode}) => {
   console.log("template.tsx rendered...")
+    const isLoggedIn = false;
+    if(!isLoggedIn){
+      
+      redirect("/login")
+    }
   return (
     <div className='space-y-5 m-5'>
       <h1>DashboardLayout</h1>
--- advanced-routing/src/app/dashboard/page.tsx
+++ advanced-routing/src/app/dashboard/page.tsx
@@ -1,7 +1,14 @@
+
+import { redirect } from 'next/navigation';
 import React from 'react'
 
 const page = () => {
   console.log("dashboard home page rendered...")
+  const isLoggedIn = false;
+  if(!isLoggedIn){
+    
+    redirect("/login")
+  }
   return (
     <div>
       Dashboard
