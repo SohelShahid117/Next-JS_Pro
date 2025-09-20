235a419eb2ca460e9e20c6162f0489819366ebdf
SohelShahid117 <sohelshahid09@gmail.com>
2025-09-20 23:47:57 +0600

    685. Intercepting Routes part-3 done

--- advanced-routing/src/app/dashboard/settings/page.tsx
+++ advanced-routing/src/app/dashboard/settings/page.tsx
@@ -1,8 +1,16 @@
+import Link from "next/link";
 import React from "react";
 
 const page = () => {
-  console.log("setting page rendered...")
-  return <div>Settings page</div>;
+  console.log("setting page rendered...");
+  return (
+    <div>
+      <h2>Settings page</h2>{" "}
+      <Link href="/image/1235" className="bg-amber-50 px-5 py-2 mt-2">
+        go to Image-123
+      </Link>
+    </div>
+  );
 };
 
 export default page;
